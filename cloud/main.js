435da82488b6679dd1a3f2a2e5484b48c28b67dd
Sibi <sibi.sakthivel@mastree.io>
2021-03-19 23:29:12 +0530

    Added check to avoid undefined userIds

--- cloud/main.js
+++ cloud/main.js
@@ -1,45 +1,47 @@
 const query = new Parse.Query('SessionLogs');
 const SUBSCRIBE = 'subscribe';
 const DISCONNECT = 'ws_disconnect';
-const UNSUBSCRIBE = 'unsubscribe'
+const UNSUBSCRIBE = 'unsubscribe';
 
 const updateSessionLogs = async (value, queryName) => {
-  query.equalTo('sessionId', queryName.sessionId);
-  query.equalTo('userId', queryName.userIds);
-  query
-    .find()
-    .then(results => {
-      // If there is no entry for that session and user
-      if (results.length == 0) {
-        let SessionLogs = Parse.Object.extend('SessionLogs');
-        let sessionLog = new SessionLogs();
-        sessionLog.set('sessionId', queryName.sessionId);
-        sessionLog.set('userId', queryName.userIds);
-        let data = [];
-        data.push(value);
-        sessionLog.set('action', data);
-        // creating log object
-        sessionLog.save().then(
-          log => {
-            console.log('New object created with objectId: ' + log.id);
-          },
-          error => {
-            console.log('Failed to create new object, with error code: ' + error.message);
-          }
-        );
-      } else {
-        // Update the action if the unique entry for user and session exists
-        // Ideally there will be only one row, handled to loop over to avoid the issue
-        results.forEach(log => {
-          log.add('action', value);
-          log.save();
-          console.log('updated the object with sessionLogs entry');
-        });
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  if (queryName.sessionId && queryName.userIds) {
+    query.equalTo('sessionId', queryName.sessionId);
+    query.equalTo('userId', queryName.userIds);
+    query
+      .find()
+      .then(results => {
+        // If there is no entry for that session and user
+        if (results.length == 0) {
+          let SessionLogs = Parse.Object.extend('SessionLogs');
+          let sessionLog = new SessionLogs();
+          sessionLog.set('sessionId', queryName.sessionId);
+          sessionLog.set('userId', queryName.userIds);
+          let data = [];
+          data.push(value);
+          sessionLog.set('action', data);
+          // creating log object
+          sessionLog.save().then(
+            log => {
+              console.log('New object created with objectId: ' + log.id);
+            },
+            error => {
+              console.log('Failed to create new object, with error code: ' + error.message);
+            }
+          );
+        } else {
+          // Update the action if the unique entry for user and session exists
+          // Ideally there will be only one row, handled to loop over to avoid the issue
+          results.forEach(log => {
+            log.add('action', value);
+            log.save();
+            console.log('updated the object with sessionLogs entry');
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 };
 
 const liveQueryTrigger = async (event, className, queryName, currentTime) => {
@@ -48,7 +50,11 @@ const liveQueryTrigger = async (event, className, queryName, currentTime) => {
     if (event == SUBSCRIBE && className == 'Session') {
       let value = 'c:' + currentTime;
       updateSessionLogs(value, queryName);
-    } else if ((event == DISCONNECT || event == UNSUBSCRIBE) && queryName && ('Session' in queryName)) {
+    } else if (
+      (event == DISCONNECT || event == UNSUBSCRIBE) &&
+      queryName &&
+      'Session' in queryName
+    ) {
       let value = 'd:' + currentTime;
       updateSessionLogs(value, queryName['Session']);
     }
@@ -73,11 +79,9 @@ Parse.Cloud.onLiveQueryEvent(
     queryName,
   }) => {
     console.log('--------------- current total connections ------------', clients);
-    console.log(event, className)
-    console.log(queryName)
-    if (
-      (event == SUBSCRIBE || event == DISCONNECT || event == UNSUBSCRIBE) && queryName
-    ) {
+    console.log(event, className);
+    console.log(queryName);
+    if ((event == SUBSCRIBE || event == DISCONNECT || event == UNSUBSCRIBE) && queryName) {
       let currentTime = Date.now();
       liveQueryTrigger(event, className, queryName, currentTime);
     }
